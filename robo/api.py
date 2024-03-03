29ef63b583eecf741c69305d3c006265f6882106
ktf <not.ktf@proton.me>
2024-03-03 14:20:19 -0800

    Add some documentation, Python-style

--- robo/api.py
+++ robo/api.py
@@ -3,10 +3,16 @@ from robo.extractor import Result
 
 
 def extract_one(extractor_name: str, key: str, local_testing: bool) -> list[Result]:
+    """Fetch the given `Extractor` by name and use it to extract data for the
+    given search key. If `local_testing` is `True`, attempt to use local test
+    data instead of hitting a remote source."""
     extract = extractors.get_extractor(extractor_name)
     return extract(key, local_testing)
 
 
 # TODO!
 def extract_list(extractor_name: str, key: str, local_testing: bool) -> list[Result]:
+    """Fetch the given `Extractor` by name and use it to extract data for the
+    given list of search keys. If `local_testing` is `True`, attempt to use
+    local test data instead of hitting a remote source."""
     return []
--- robo/extractor/web.py
+++ robo/extractor/web.py
@@ -9,6 +9,9 @@ DEFAULT_PARSER: str = "html.parser"
 def get_page_data(
     key: str, url: str, local_testing: bool, local_path: str, encoding: str = "utf-8"
 ) -> BeautifulSoup:
+    """Gets parsed web page data, either from a remote URL or local test files.
+    For now, `local_path` is relative to `tests/testdata/`. Within
+    `local_path`, we look for a file called `{key}.html`."""
     if local_testing:
         soup = get_local_page_data(key, local_path, encoding)
     else:
@@ -17,15 +20,15 @@ def get_page_data(
 
 
 def get_remote_page_data(url: str) -> BeautifulSoup:
+    """Fetch the page at the given URL and parse with `BeautifulSoup`."""
     page = requests.get(url)
     soup = BeautifulSoup(page.content, DEFAULT_PARSER)
     return soup
 
 
-# Get local HTML data instead of fetching from a URL. Look for a filename that
-# matches the input "key" (with `.html` extension) in a directory that matches
-# the extractor module path.
 def get_local_page_data(key: str, path: str, enc: str) -> BeautifulSoup:
+    """Get local HTML data instead of fetching from a URL. Look for a file
+    called `{key}.html` in the given local path."""
     # TODO: cleanup/normalize path concatenation
     newpath = "tests/testdata/" + path + f"/{key}.html"
     with open(newpath, encoding=enc) as fp:
