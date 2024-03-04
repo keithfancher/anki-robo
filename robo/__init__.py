eea8187015d86edc4cfdca0f2a4fcd63d2b5a329
ktf <not.ktf@proton.me>
2024-03-04 13:18:32 -0800

    Add ability to extract terms from a plaintext list

--- robo/__init__.py
+++ robo/__init__.py
@@ -1,5 +1,11 @@
 from robo.api import extract_list, extract_one
 from robo.extractors import get_extractor_names
+from robo.input import from_plaintext
 
 # The public API of the `robo` library.
-__all__ = ["get_extractor_names", "extract_list", "extract_one"]
+__all__ = [
+    "extract_list",
+    "extract_one",
+    "get_extractor_names",
+    "from_plaintext",
+]
---
+++ robo/input.py
@@ -0,0 +1,9 @@
+import robo
+from robo.extractor import Result
+
+
+def from_plaintext(text: str, extractor_name: str, local_testing: bool) -> list[Result]:
+    """Extract data from the given source given a plaintext, newline-delimited
+    list of words."""
+    keys = text.splitlines()
+    return robo.extract_list(extractor_name, keys, local_testing)
