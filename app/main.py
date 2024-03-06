659d6d270f24a707a4e01fa2b909af7622fd0b03
ktf <not.ktf@proton.me>
2024-03-06 14:12:52 -0800

    Beginnings of a proper CLI app, proper CLI args

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import robo
 
 
@@ -6,3 +8,42 @@ def from_text_file(filename: str, extractor_name: str, local_testing: bool) -> s
         contents = f.read()
     results_summary = robo.from_plaintext(contents, extractor_name, local_testing)
     return robo.to_csv(results_summary.results)
+
+
+def list_extractors_callback(args: argparse.Namespace) -> None:
+    for extractor_name in robo.get_extractor_names():
+        print(extractor_name)
+
+
+def extract_callback(args: argparse.Namespace) -> None:
+    local_testing = True  # TODO!
+    try:
+        r = from_text_file(args.infile, args.extractor, local_testing)
+        print(r)
+    except robo.InvalidExtractorName:
+        print("Invalid extractor name: " + args.extractor)
+    except FileNotFoundError:
+        print("Input file not found: " + args.infile)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(help="Available anki-robo commands")
+
+    # `list` subcommand: show all available extractors
+    parser_list = subparsers.add_parser(
+        "list", aliases=["l"], help="List all available data extractors"
+    )
+    parser_list.set_defaults(func=list_extractors_callback)
+
+    # `get` subcommand: get Anki data with a given extractor
+    parser_get = subparsers.add_parser(
+        "get", aliases=["g"], help="Get Anki card data with the given extractor"
+    )
+    parser_get.add_argument("extractor")
+    parser_get.add_argument("infile")
+    parser_get.set_defaults(func=extract_callback)
+
+    args = parser.parse_args()
+    args.func(args)
--- robo-run.py
+++ robo-run.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
 
 
-import sys
-
 import app.main as app
 
-
-def main():
-    if len(sys.argv) == 2:
-        extractor = "linguee-fr"
-        r = app.from_text_file(sys.argv[1], extractor, local_testing=True)
-        print(r)
-    else:
-        print("Usage: robo.py [FILE_NAME]")
-
-
 if __name__ == "__main__":
-    main()
+    app.main()
